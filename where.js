95d1447b4b91e5a9529e33eb9fe465a63ac3673f
Mineev Alexander <mineev.buryat@gmail.com>
2017-04-19 09:33:11 +0800

    first commit for public

---
+++ where.js
@@ -0,0 +1,16 @@
+myData = [
+[52.2685014,104.3833366, 'International Airport Irkutsk, ul. Shiryamova, 13, Irkutsk, Irkutskaya oblast, Russia, 664000'],
+[52.0218937,113.307066, 'Chita Airport, Zvezdnaya ul., 17, Chita, Zabaykalskiy kray, Russia, 672018'],
+[62.086467,129.750195, 'Привокзальная площадь аэропорта, 10, 2 этаж, каб. 2-157, Yakutsk, Sakha Republic, Russia, 677014'],
+[55.0113541,82.652163, 'Novosibirsk International Airport (Tolmachevo), Ob, Novosibirskaya oblast, Russia, 633104'],
+[56.1767877,92.4813965, 'Yemelyanovo International Airport, Krasnoyarskiy kray, Russia, 663021'],
+[59.621214,150.9180141, 'Magadan-13 Airport, Magadan, Magadanskaya oblast, Russia, 685030'],
+[55.4103069,37.9024513, 'Domodedovo Moscow Airport, Moskovskaya oblast, Russia'],
+[37.4601908,126.4406957, 'Incheon International Airport, 272 Gonghang-ro, Jung-gu, Incheon, South Korea'],
+[50.4221882,127.4074683, 'Ignatievo Airport, Ignatyevskaya Rd, Amur Oblast, Russia, 675019'],
+[43.3948533,132.1477422, 'Vladivostok International Airport, Портовая ул., 45, Artem, Primorskiy kray, Russia'],
+[56.3632155,114.9308802, 'Aeroport Taksimo, Бурятия Респ., Russia, 671560'],
+[55.7954176,109.5783548, 'Nizhneangarsk, aeroport OAO "Aviakompaniya "Buryatskiye avialinii", 143, ул. Рабочая, пгт. Нижнеангарск, Республика Бурятия, Россия 671710, Buryatiya Republits, Russia, 671710'],
+[40.0798573,116.6031121, 'Beijing Capital International Airport, Shunyi, Beijing Shi, China'],
+[55.7954176,109.5783548, 'Nizhneangarsk, aeroport OAO "Aviakompaniya "Buryatskiye avialinii", 143, ул. Рабочая, пгт. Нижнеангарск, Республика Бурятия, Россия 671710, Russia']
+];
